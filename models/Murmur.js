76e97e853142a6907b948b171a103ca350a8c60b
Philipp Strauß <phil13.strauss@gmail.com>
2022-06-20 14:18:30 +0200

    add schema for User and Murmur

---
+++ models/Murmur.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const murmurSchema = new Schema({   
+    user_id: String,
+    city: {type:String, required: true},
+    tip: {type:String, required: true},
+    picture: String,
+    address: String,
+    date: {type: Date, default: Date.now},
+    tags: [{type:String, required: true}],
+    comments: [{username: String, body: String, date: Date}],
+    likes: {upvotes: [{username: String}], downvotes:[{username: String}]},
+    });
+
+export default model("Murmur", murmurSchema);
