7f3eb88d0ade0163d2d84218fbb18d99abb95a20
Mehtap <kayhanmehtap10@gmail.com>
2022-06-22 10:55:07 +0200

    modify register and implement jsonwebtoken & bcrypt

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,19 +1,42 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const createNewUser = async (req, res) => {
-    try {
-      const { username, email, password, picture, accountlikes } = req.body;
-      const newUser = await User.create({ username, email, password, picture, accountlikes });
-      res.status(201).json(newUser);
-    } catch (error) {
-      res.status(500).json(error);
+  try {
+    const { username, email, password, picture } = req.body;
+
+    const emailExists = await User.findOne({ email: req.body.email });
+
+    if (emailExists) {
+      res.status(400).send("Email already in use, please log in.");
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+        picture,
+      });
+      const token = jwt.sign({ email: newUser.email }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      if (token && newUser) {
+        res
+          .status(201)
+          .set("Authorization", token)
+          .send("User successfully created");
+      }
     }
-  };
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const getUser = async (req, res) => {
   try {
     const { username } = req.params;
-    const user = await User.findOne({ username: username}).exec();
+    const user = await User.findOne({ username: username }).exec();
     res.status(200).json(user);
   } catch (error) {
     res.status(404).json(error);
