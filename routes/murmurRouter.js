49540d53f16bce4abfeaa0bb3af2711003fde558
Paul Ludwigs <paul@sisyfox.com>
2022-07-05 14:01:46 +0200

    add endpoint for writing comments

--- routes/murmurRouter.js
+++ routes/murmurRouter.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+    createComment,
     createNewMurmur, downvoteMurMur, getMurmurByCity, getMurmurById, upvoteMurMur,
 } from "../controllers/murmurControllers.js";
 
@@ -13,6 +14,8 @@ murmurRouter.route("/id/:id").get(getMurmurById);
 
 murmurRouter.route("/upvote").put(upvoteMurMur);
 murmurRouter.route("/downvote").put(downvoteMurMur);
+
+murmurRouter.route("/comment/:id").put(createComment);
 
 
 export default murmurRouter;
